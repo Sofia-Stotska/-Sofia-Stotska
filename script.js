bcae21c2de91e5fb618730e6885611910512849e
Sofia-Stotska <sofiastotska6@gmail.com>
2026-03-16 14:46:26 +0200

    add files

---
+++ script.js
@@ -0,0 +1,19 @@
+// let a = Number (prompt("Введіть перше число"))
+// let b = Number (prompt("Введіть друге число"))
+// let c = Number (prompt("Введіть третє число"))
+// let average = (a + b + c) / 3;
+// console. log("Середнє арифметичне: " + average);
+
+// console. log("Модуль числа: " + Math.abs(average));
+// console. log("Округлене у більшу сторону: " + Math. ceil (average));
+// console. log("Округлене у меншу сторону: " + Math. floor (average));
+// console. log("Округлене у меншу сторону: " + Math. pow (average,2));
+
+// console. log ("Чи ділиться число на 5 без остачі?." + ( average % 5 === 0));
+
+// if (a + b > c && b + c > a) {
+//     console. log ( "Трикутник може існувати за заданими сторонами.");
+// } else {
+//     console. log ("Трикутник не може існувати за заданими сторонами.");
+// }
+
