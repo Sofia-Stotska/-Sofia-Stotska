a4e8fa0a62a029c04253842027b6c55e35f2dee6
Sofia-Stotska <sofiastotska6@gmail.com>
2026-03-20 15:35:05 +0200

    Add commented JavaScript tasks in script2.js

---
+++ script2.js
@@ -0,0 +1,56 @@
+//  //завдання 
+//   let age = Number(prompt("Введіть ваш вік:"));
+
+
+//  if (age < 18 && age > 0) {
+//      alert("Вам заборонено вхід")
+//  } else if (age >= 18 && age <= 65) {
+//     alert("Ласкаво просимо!");
+//  } else {
+//      alert("Будь ласка, будьте обережні!");
+//  }
+
+// //Завдання 2
+
+// let n = Number(prompt("Введіть число:"));
+
+//  for (let i = 2; i <= n; i+=2) {
+//    console.log(i);
+// }
+
+// //Завдання 3
+// // let n = Number(prompt("Введіть число n:"));
+
+// //
+// // let result = 1;
+// // let i = 1;
+// // while (i <=n) {
+// //     result *= i;
+// //     i++;
+// // }
+// // console.log("Факторіал:"+ n +"дорівнює" + result);
+// //Завдання 4
+// let a = Number(prompt("Введіть перше число:"));
+// let b = Number(prompt("Введіть друге число:"));
+// let operation = prompt("Введіть операцію (+, -, *, /):");
+
+// switch (operation) {
+//     case "+":
+//        alert (result = a + b);
+//         break;
+//     case "-":
+//        alert (result = a - b);
+//         break;
+//     case "*":
+//       alert ( result = a * b);
+//         break;
+//      case "*":
+//       alert ( result = a / b);
+//         break;   
+
+//     default:
+//         result = "Невідома операція"; 
+// }
+
+
+
